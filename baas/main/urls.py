036f07886cfecfc5a540a40251fd2a1e37e83b2d
Keerthivasan-s-j <jagankeerthisri@gmail.com>
2025-04-18 18:52:23 +0530

    ✨ feat: add contact and shop page

--- baas/main/urls.py
+++ baas/main/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.landingpage, name="landingpage"),
     path('about/', views.about, name="about"),
+    path('shop/', views.shop, name="shop"),
+    path('contact/', views.contact, name="contact"),
 ]
--- baas/main/views.py
+++ baas/main/views.py
@@ -5,3 +5,9 @@ def landingpage(request):
 
 def about(request):
     return render(request, "main/about.html")
+
+def shop(request):
+    return render(request, "main/shop.html")
+
+def contact(request):
+    return render(request, "main/contact.html")
