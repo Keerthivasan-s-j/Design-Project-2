601a0589744468cf79ee7560e4a114faf6653719
Keerthivasan-s-j <jagankeerthisri@gmail.com>
2025-04-19 17:51:46 +0530

    ✨ feat: add sign in page

--- baas/main/urls.py
+++ baas/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.landingpage, name="landingpage"),
+    path('signin/', views.signin, name="signin"),
     path('about/', views.about, name="about"),
     path('shop/', views.shop, name="shop"),
     path('contact/', views.contact, name="contact"),
--- baas/main/views.py
+++ baas/main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+def signin(request):
+    return render(request, "main/signin.html")
+
 def landingpage(request):
     return render(request, "main\landingpage.html")
 
